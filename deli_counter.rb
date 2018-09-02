d32aa50c705da9d4c4f9dda4cd9440419051a875
Leslie Turis <lturis@gmail.com>
2018-09-02 03:50:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,20 @@
 katz_deli = []
 
 def line(katz_deli)
-  current_line = []
   if katz_deli.empty?
     puts 'The line is currently empty.'
   else
-    katz_deli.each.with_index(1) do |name, index|
-      current_line.push("#{index}. #{name}")
+    current_line = "The line is currently: "
+    katz_deli.each.with_index(1) do |name, i|
+      current_line << "#{i}. #{name}"
     end
-  puts "The line is currently: #{current_line.join(' ')}"
+   puts current_line
   end
 end
 
 
 def take_a_number(katz_deli, name)
-  katz_deli << "#{name}"
+  katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
